aa8818b41a7bab6ed2473d6d4351f34b027aea4d
Vanessa <>
2018-09-20 18:02:05 -0500

    work on table of contents for week 1

---
+++ week_1/contents_test.py
@@ -0,0 +1,22 @@
+contents = {
+	"chap-1" : {
+	    "chapter" : "Chapter 1",
+	    "name" : "Fancy Cats",
+	    "page" : "1",
+	},
+	"chap_2" : {
+	    "chapter" : "Chapter 2",
+	    "name" : "Fat Cats",
+	    "page" : "32",
+	},
+	"chap_3" : {
+	    "chapter" : "Chapter 3",
+	    "name" : "Hairless Cats",
+	    "page" : "53",
+	},
+	"chap_4" : {
+	    "chapter" : "Chapter 4",
+	    "name" : "Fluffy Cats",
+	    "page" : "75",
+	},
+}
