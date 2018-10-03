8a7fdd2c918fdf3d8271c2676b9e0e1396c199f1
Vanessa <>
2018-10-03 17:38:16 -0500

    Finished new style roman numeral problem

--- week_1/soc-wk01-cert-venessa-stewart.py
+++ week_1/soc-wk01-cert-venessa-stewart.py
@@ -228,6 +228,22 @@ def make_roman(number):
 
 print("Your Roman Numeral is " + make_roman(number) + ".")
 	
+#Roman Numerals new style
+
+number = int(input("Enter number between 1 and 3000: "))
+
+numerals = (("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100), ("XC", 90), ("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1))
+
+def make_roman(number):
+    result = ''
+    for roman, n in numerals:
+    	if n<= number:
+    		result += roman * (number // n)
+    		number %= n
+    return result
+
+print("Your Roman Numeral is " + make_roman(number) + ".")
+
 
 
 
--- week_1/test2.py
+++ week_1/test2.py
@@ -1,6 +1,6 @@
 number = int(input("Enter number between 1 and 3000: "))
 
-numerals = (("M", 1000), ("D", 500), ("C", 100), ("L", 50), ("X", 10), ("V", 5), ("I", 1))
+numerals = (("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100), ("XC", 90), ("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1))
 
 def make_roman(number):
     result = ''
@@ -10,4 +10,4 @@ def make_roman(number):
     		number %= n
     return result
 
-make_roman(number) 
+print("Your Roman Numeral is " + make_roman(number) + ".")
