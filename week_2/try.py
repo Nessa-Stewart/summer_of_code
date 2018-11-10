17e75d3bb1791127dcd4a5f15516298fc2da31f7
Vanessa <>
2018-11-10 14:37:37 -0600

    updated try.py

--- week_2/try.py
+++ week_2/try.py
@@ -7,14 +7,14 @@ raw = file.read()
 
 raw = raw.lower()
 
-raw = [list(line.rstrip()) for line in raw]
+#raw = [list(line.rstrip()) for line in raw]
 
 alphabet = ['abcdefghijklmnopqrstuvwxyz']
 alphabet = [list(line.rstrip()) for line in alphabet]
 letterfreq = []
 letter =[]
 
-letter.append(raw)
+#letter.append(raw)
 
 #print(letter)
 #print(alphabet)
@@ -30,6 +30,16 @@ letter.append(raw)
 
 
 # print(hits)
+
+def count_letters(text):
+	for char in alphabet:
+		count=0
+		for c in letter:
+			if c == char:
+				count+=1
+		print(char, str(count))
+
+count_letters(raw)
 
 
 
