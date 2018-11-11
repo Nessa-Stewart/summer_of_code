cb5bab783f10e3bb2dddd9e4365766b884a0c0e9
Nessa Stewart <penessa@gmail.com>
2018-11-10 20:55:01 -0600

    completed day 1 homework

--- week_2/soc-wk02-cert-venessa-stewart.py
+++ week_2/soc-wk02-cert-venessa-stewart.py
@@ -6,9 +6,27 @@
 filename = "alice_in_wonderland.txt"
 file = open(filename, "r")
 
-alphabets = "abcdefghijklmnopqrstuvwxyz"
+alphabet = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z']
+letter =[]
+letterfreq = []
+
+def count_letters(text):
+	text=text.lower()
+	for char in alphabet:
+		count=0
+		for c in raw:
+			if c == char:
+				count+=1
+		letter.append(char)
+		letterfreq.append(str(count))
 
 raw = file.read()
 
-raw.count(alphabets)
+
+count_letters(raw)
+
+alphacount=list(zip(letter, letterfreq))
+
+
+print(alphacount)
 	
--- week_2/try.py
+++ week_2/try.py
@@ -1,45 +1,27 @@
 filename = "alice_in_wonderland.txt"
 file = open(filename, "r")
 
+alphabet = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z']
+letter =[]
+letterfreq = []
 
+def count_letters(text):
+	text=text.lower()
+	for char in alphabet:
+		count=0
+		for c in raw:
+			if c == char:
+				count+=1
+		letter.append(char)
+		letterfreq.append(str(count))
 
 raw = file.read()
 
-raw = raw.lower()
-
-#raw = [list(line.rstrip()) for line in raw]
-
-alphabet = ['abcdefghijklmnopqrstuvwxyz']
-alphabet = [list(line.rstrip()) for line in alphabet]
-letterfreq = []
-letter =[]
-
-#letter.append(raw)
-
-#print(letter)
-#print(alphabet)
-
-
-
-# hits = [
-# 	(alphabet[i], letter.count(alphabet[i]))
-# 	for i in range(len(alphabet))
-# 	if letter.count(alphabet[i])
-# ]
-
-
-
-# print(hits)
-
-def count_letters(text):
-	for char in alphabet:
-		count=0
-		for c in letter:
-			if c == char:
-				count+=1
-		print(char, str(count))
 
 count_letters(raw)
 
+alphacount=list(zip(letter, letterfreq))
 
+
+print(alphacount)
 
